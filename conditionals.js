4e1b01a8c1a564c71ba60918ce201d36ad196e9c
Mohamed Ali <12maali27@gmail.com>
2021-11-04 16:20:58 +0000

    adding changes from conditionals.js, create and edited files ice-cream.js and packing-list.js

--- conditionals.js
+++ conditionals.js
@@ -1,23 +1,23 @@
-// const raining = true;
-// const cold = true;
+const raining = true;
+const cold = true;
 
-// if (raining) {
-//   console.log("Don't forget your umbrella!");
-// }
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
 
-// if (cold) {
-//   console.log("Make sure you pick out a scarf!");
-// }
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
 
-// console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");
 
-// const cold = true;
+const cold = true;
 
-// if (cold) {
-//   console.log("Make sure you pick out a scarf!");
-// } else {
-//   console.log("Short sleeves are fine.");
-// }
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
 
 const temperature = 12;
 
@@ -30,3 +30,19 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
